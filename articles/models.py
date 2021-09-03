a4fc68fce115a1c02721914193fad80bd58cd842
Mate Rusz <ruszmate33@gmail.com>
2021-09-03 16:22:48 +0200

    slugify refactored into utils and save-pre-post-save modified

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,8 @@
-import random
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.utils import timezone
-from django.utils.text import slugify
+
+from .utils import slugify_instance_title
 
 # Create your models here.
 class Article(models.Model):
@@ -18,35 +18,19 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         # if self.slug is None:
         #     self.slug = slugify(self.title) # beware of nonunique titles!
-        if self.slug is None:
-            slugify_instance_title(self, save=False)
+        # if self.slug is None:
+        #     slugify_instance_title(self, save=False)
         super().save(*args, **kwargs)
         # super().save(*args, **kwargs) is equivalent to:
         # obj = Article.object.get(id=1)
         # obj.save()
+   
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    if instance.slug is None:
+        slugify_instance_title(instance, save=False)
 
-def slugify_instance_title(instance, save=False, new_slug=None):
-    if new_slug is not None:
-        slug = new_slug
-    else:
-        slug = slugify(instance.title)
-    Klass = instance.__class__
-    qs = Klass.objects.filter(slug=slug).exclude(id=instance.id)
-    if qs.exists():
-        random_int = random.randint(200_000, 500_000)
-        slug = f"{slug}-{random_int}"
-        return slugify_instance_title(instance, save=False, new_slug=slug)
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance # not completely necessary
-    
-# def article_pre_save(sender, instance, *args, **kwargs):
-#     print('pre_save')
-#     if instance.slug is None:
-#         slugify_instance_title(instance, save=False)
-
-# pre_save.connect(article_pre_save, sender=Article)
+pre_save.connect(article_pre_save, sender=Article)
 # alternatively with the reveiver decorator
 
 def article_post_save(sender, instance, created, *args, **kwargs):
