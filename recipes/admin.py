5654bce4953a5927fe5be22116564ff87641296c
Mate Rusz <ruszmate33@gmail.com>
2021-09-07 17:13:50 +0200

    recipe app added and Admin inlines for foreignKeys (User-Recipe-Ingredient)

---
+++ recipes/admin.py
@@ -0,0 +1,51 @@
+from django.contrib.auth import get_user_model
+from django.contrib import admin
+
+# Register your models here.
+from .models import Recipe, RecipeIngredient
+
+User = get_user_model() # beyond the models.py this is how you get the userModel
+admin.site.unregister(User) # we will need to unregister to register again with UserAdmin
+
+admin.site.register(RecipeIngredient)
+
+# class UserInline(admin.TabularInline):
+#     model = User
+#
+# class RecipeAdmin(admin.ModelAdmin):
+#     inlines = [UserInline, RecipeIngredientInline]
+#     list_display = ['name', 'user']
+#     readonly_fields = ['timestamp', 'updated']
+#     raw_id_fields = ['user']
+# ERRORS:
+# <class 'recipes.admin.UserInline'>: (admin.E202) 'auth.User' has no ForeignKey to 'recipes.Recipe'.
+# we need to Tab other things, since this is the highest in hierarchy
+# user > recipe > recipeIngredient
+# User-TabularInline -> Recipe-TabularInline
+
+
+class RecipeInline(admin.TabularInline):
+    model = Recipe
+    extra = 0
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [RecipeInline]
+    list_display = ['username']
+
+admin.site.register(User, UserAdmin)
+
+    
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    # fields = ['name', 'quantity', 'unit', 'directions']
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+
+admin.site.register(Recipe, RecipeAdmin)
+
---
+++ recipes/models.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.db import models
+
+
+"""
+-Global
+    - Ingredients
+    - Recipes
+- User
+    - Ingredients
+    - Recipes
+        - Ingredients
+        - Directions for Ingredients
+"""
+
+User = settings.AUTH_USER_MODEL
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated =  models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated =  models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    # class RecipeImage():
+    #     recipe = models.ForeignKey(Recipe)
+
