d56515af45712e013c1e28e93cc674c02a4f9c73
Mate Rusz <ruszmate33@gmail.com>
2021-09-08 12:25:37 +0200

    custom validation of RecipeIngredient units with 'pint'

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from .validators import validate_unit_of_measure
 
 
 """
@@ -22,14 +23,14 @@ class Recipe(models.Model):
     directions = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated =  models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+    updated = models.BooleanField(default=True)
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     quantity = models.CharField(max_length=50)
-    unit = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50, validators=[validate_unit_of_measure]) # 'pounds', 'lbs', 'oz', 'gram'
     directions = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated =  models.DateTimeField(auto_now=True)
